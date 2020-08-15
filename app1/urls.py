599f241479c38fcbf6a12d1317644282775ae631
Atharva kulkarni <atharvaanantkulkarni@gmail.com>
2020-08-15 12:24:00 +0530

    f2

--- app1/urls.py
+++ app1/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.page1,name='page1'),
     path('page2/',views.login,name='login'),
-    path('/page3/',views.logout,name='logout')
+    path('page3/',views.logout,name='logout')
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import info
-from django.contrib.auth import authenticate,login,logout
+from django.contrib import auth
 from django.contrib.auth.models import User
 
 def page1(request):
@@ -19,9 +19,10 @@ def page1(request):
 				x=User.objects.get(username=uname)
 				return HttpResponse('<h2>User already exists</h2>')
 			except User.DoesNotExist:
-				profile = User.objects.create_user(username=uname,password=pd,email=email,first_name=fname,last_name=lname)
-				profile=Profile(pno=pno)
-				profile.save()
+				user= User.objects.create_user(username=uname,password=pd,email=email,first_name=fname,last_name=lname)
+				user.save()
+				info=info(user=user,pno=pno)
+				info.save()
 				return render(request,'page2.html')
 		else:
 			return render(request,'page1.html',{'message':'Passwords do not match'})
@@ -34,17 +35,17 @@ def login(request):
 	if request.method=='POST':
 		uname1=request.POST.get('uname')
 		pd1=request.POST.get('pd')
-		user=authenticate(username=uname1,password=pd1)
+		user=auth.authenticate(username=uname1,password=pd1)
 		if user is None:
 			return render(request,'page2.html',{'message':'Invalid credentials'})
 		else:
-			login(request)
+			q=auth.login(request,user)
 			return render(request,'page3.html',{'message':'User logged in'})
 	else:
 		return render(request,'page2.html')
 
 def logout(request):
 	if request.method=='POST':
-		logout(request)
+		auth.logout(request)
 		return render(request,'page1.html',{'message':'Logged out successfully!'})
 
